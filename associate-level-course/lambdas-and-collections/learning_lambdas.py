98e29aaeea4fc45ca9fc1837de10b3d90ed2aa41
Jose Ogalde <jose.ogalde@alma.cl>
2021-01-13 00:19:47 -0300

    lambda function example

--- associate-level-course/lambdas-and-collections/learning_lambdas.py
+++ associate-level-course/lambdas-and-collections/learning_lambdas.py
@@ -2,5 +2,8 @@ def square(num):
     return num * num
 
 square_lambda = lambda num: num * num
+copy_of_lambda = lambda num: num * num
+
 
 assert square(7) == square_lambda(7)
+assert id(square_lambda) != id(copy_of_lambda)
