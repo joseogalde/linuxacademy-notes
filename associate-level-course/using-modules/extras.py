d45c9303225261cb8d5e6191a673cb8485c207ed
Jose Ogalde <jose.ogalde@alma.cl>
2021-01-14 02:14:58 -0300

    class exampels of defining and using modules

---
+++ associate-level-course/using-modules/extras.py
@@ -0,0 +1,5 @@
+import helpers
+
+if __name__ == "__main__":
+    name = "Huachimingo"
+    print(f"__name__ in helpers.py: {__name__}")
---
+++ associate-level-course/using-modules/main.py
@@ -0,0 +1,7 @@
+from helpers import extract_lower, extract_upper
+
+import extras
+
+name = "Jose"
+print(f"Lowercase letters: {extract_lower(name)}")
+print(f"Uppercase letters: {extract_upper(name)}")
